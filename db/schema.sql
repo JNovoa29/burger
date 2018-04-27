a78eae14a872d84924d1ba928cdd2bb8ab607755
Jordan Novoa <jordan.novoa15@gmail.com>
2018-04-26 18:37:17 -0700

    initial files, db folder

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+-- create burgers database
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+-- create a table with the fields id, burger_name = string, and devoured = boolean
+CREATE TABLE burgers (
+id AUTO_INCREMENT PRIMARY KEY NOT NULL,
+burger_name VARCHAR(50) NOT NULL,
+devoured BOOLEAN NOT NULL
+);
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+-- queries to pop burger table with 3 entries minimum
+INSERT INTO burgers (burger_name, devour)
+VALUES ('Volcano Burger', false), ('Salmon Burger', false), ('Kawaii Burger', false);
